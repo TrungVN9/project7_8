5cfcca28375d7a2d5d79f8bd3077b1a94468f164
TrungVN9 <trungnvong956@g.ucla.edu>
2026-03-26 23:41:34 -0700

    added output .asm

---
+++ bin/Sys.asm
@@ -0,0 +1,64 @@
+// function Sys.init 0
+(Sys.init)
+// push constant 4
+@4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// call Main.fibonacci 1
+@Sys.init$ret.0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+D=M
+@6
+D=D-A
+@ARG
+M=D
+@SP
+D=M
+@LCL
+M=D
+@Main.fibonacci
+0;JMP
+(Sys.init$ret.0)
+// label END
+(Sys.init$END)
+// goto END
+@Sys.init$END
+0;JMP
